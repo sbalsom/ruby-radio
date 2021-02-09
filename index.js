5d283d6cc5c15eb5fd74cb7dfdc3239e435488c8
Sadie Balsom <saralogsin@protonmail.com>
2021-02-09 21:47:12 +0100

    adds tone.js

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-window.addEventListener('load', () => {
-    const sounds = document.querySelectorAll('.sound');
-    const buttons = document.querySelectorAll('.button');
-    sounds.forEach((sound) => {
-        sound.loop = true;
-        sound.muted = true;
-        console.log(sound.duration)
-    });
-    sounds.forEach((sound) => {
-        sound.play();
-    });
-    buttons.forEach((button, index) => {
-        
-        button.addEventListener('click', () => {
-            let sound = sounds[index]
-            button.classList.toggle('active')
-            if (sound.muted == true) {
-                sound.muted = false;
-            } else if (sound.muted == false) {
-                sound.muted = true;
-            }
-            
-            
-        });
+
+const sounds = document.querySelectorAll('.sound');
+const buttons = document.querySelectorAll('.button');
+const players = Array.from(sounds).map(sound => new Tone.Player(new Tone.Buffer(sound.src)).toDestination());
+const startButton = document.getElementById('start-button');
+
+players.forEach((player)=> {
+     player.loop = true;
+     player.mute = true;
+ })
+
+
+startButton.addEventListener('click', () => {
+    console.log('clicked !')
+    players.forEach((player) => {
+        player.start();
+    })
+});
+
+buttons.forEach((button, index) => { 
+    button.addEventListener('click', () => {
+        console.log(`clicked button number ${index}`)
+        let player = players[index]
+        button.classList.toggle('active')
+        if (player.mute == true) {
+            player.mute = false;
+        } else if (player.mute == false) {
+            player.mute = true;
+        }
+                           
     });
 });
